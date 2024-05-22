d283ce832246b86cc24b42d2648a9c84b0074cb7
Nick Nicker <nick4nicker@gmail.com>
2024-05-22 23:39:25 +0200

    main: remove examples & clean up

--- src/main.cpp
+++ src/main.cpp
@@ -10,36 +10,9 @@
 
 #include "yaml-cpp/yaml.h"
 
+using std::cin, std::cout, std::endl, std::string, std::map, std::vector;
 
-using std::cin,std::cout,std::endl,std::string,std::map,std::vector;
-
-
-int main() {
+int main()
+{
     showMenu();
-
-    // YAML::Node client = YAML::LoadFile(clientFile);
-
-    // string test = client["personal"]["name"].as<string>();
-
-    // client["lastlogin"] = 129;
-
-    // std::ofstream fout(clientFile);
-
-    // fout << client;
-
-    // fout.close();   
-
-    // cout << test << endl;
-
-    // Client client1 = Client("Jasiek", "Junak");
-
-    // client1.setCredit(100);
-
-    // client1.printInfo();
-
-    // Client client2 = Client(client);
-
-    // client2.printInfo();
-
-
 }
